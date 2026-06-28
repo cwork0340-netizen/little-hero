faaa9a083a32d10911d508ff3e8e8fc9c5157a87
cwork0340-netizen <cwork0340@gmail.com>
2026-06-29 00:16:07 +0800

    Import V2 task card stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
 import './styles/v2.css'
+import './styles/taskcard-v2.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
